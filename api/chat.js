930faa24cda6236d9fdcf794f9fc2d4c97f62153
jak3ym <jakemakaling@hotmail.com>
2025-08-09 12:33:15 +1000

    main: CORS enabled.

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,15 @@
+
 export default async function handler(req, res) {
+  // Enable CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    // Preflight request
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
