777f7b753d9f09711318bb24a54cdda365805be2
jak3ym <jakemakaling@hotmail.com>
2025-08-09 12:18:17 +1000

    main: Model free.

--- api/chat.js
+++ api/chat.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        model: "openai/gpt-oss-20b",
+        model: "openai/gpt-oss-20b:free",
         messages: [{ role: "user", content: message }],
       }),
     });
